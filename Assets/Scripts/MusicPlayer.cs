7ba98afc5dc167087fa84268b6c71ceb2c715996
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-08-27 19:42:40 +0300

    feat: add new function

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -11,6 +11,8 @@ public class MusicPlayer : MonoBehaviour
     [SerializeField] private Text NumberText;
     [SerializeField] private Text PlayPauseButtonText;
 
+    [SerializeField] private Slider DurationSlider;
+
 
     [SerializeField] List<AudioClip> MusicList = new List<AudioClip>();
 
@@ -19,6 +21,14 @@ public class MusicPlayer : MonoBehaviour
     bool pause = true;
 
     int currentSong = 0;
+
+    int LenghtMinute;
+    int LenghtSecond;
+    int DurationMinute;
+    int DurationSecond;
+
+
+
     private void Awake()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
@@ -26,13 +36,21 @@ public class MusicPlayer : MonoBehaviour
 
     void Start()
     {
-
+        CalcalateMinute();
+        NumberText.text = (currentSong + 1) + ".";
+        SongNameText.text = MusicList[currentSong].name;
     }
 
     // Update is called once per frame
     void Update()
     {
-        NumberText.text = (currentSong + 1) + ".";
+        CalculateDuration();
+        CalculateSlider();
+
+        if (audioSource.time == MusicList[currentSong].length)
+        {
+            NextMusic();
+        }
 
     }
 
@@ -48,6 +66,9 @@ public class MusicPlayer : MonoBehaviour
         else
         {
             audioSource.Play();
+            CalcalateMinute();
+            NumberText.text = (currentSong + 1) + ".";
+            SongNameText.text = MusicList[currentSong].name;
             pause = false;
             PlayPauseButtonText.text = "ll";
         }
@@ -85,5 +106,34 @@ public class MusicPlayer : MonoBehaviour
 
     }
 
+    public void CalcalateMinute()
+    {
+        LenghtMinute = (int)MusicList[currentSong].length / 60;
+        LenghtSecond = (int)MusicList[currentSong].length % 60;
 
+        LenghtText.text = LenghtMinute + "." + LenghtSecond;
+
+    }
+
+    public void CalculateDuration()
+    {
+        DurationMinute = (int)audioSource.time / 60;
+        DurationSecond = (int)audioSource.time % 60;
+
+        if (DurationSecond<10)
+        {
+            DurationText.text = DurationMinute + ".0" + DurationSecond;
+        }
+        else
+        {
+            DurationText.text = DurationMinute + "." + DurationSecond;
+        }
+    }
+
+    void CalculateSlider()
+    {
+        DurationSlider.maxValue = MusicList[currentSong].length;
+        DurationSlider.value = audioSource.time;
+
+    }
 }
